5f8b1bc1dafe08cec456f58e671b38976de763c6
Vite <enwuft@gmail.com>
2020-07-18 00:07:09 +0700

    add(about): About me

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -71,16 +71,14 @@ const About = ({
         <a href="https://dev-comrbtech.netlify.app/" target="black">
           https://dev-comrbtech.netlify.app/
         </a>
-        <li>
-          All, The most of free time to learning coding, interesting of Vue.js, JavaScript, TypeScript and Rust. This for a long time to be amateur programmer loves to code.
-        </li>
       </Ul>
-      {/* <H4>Experience</H4>
-      <ResumeHeader>
-        <h5>Signal Messenger &middot; Desktop Developer &middot; Remote</h5>
-        <h5>April 2019 - Present</h5>
-      </ResumeHeader>
-      <ResumeHeader>
+      <P>
+        {' '}
+        All, The most of free time to learning coding, interesting of Vue.js,
+        JavaScript, TypeScript and Rust. This for a long time to be amateur
+        programmer loves to code.
+      </P>
+      {/* <ResumeHeader>
         <h5>SHIFT Media &middot; Front End Lead &middot; Boston, MA</h5>
         <h5>September 2017 - April 2019</h5>
       </ResumeHeader>
